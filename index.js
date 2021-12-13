5ae82021ba416f66e16ef2cc2a6f6c5970a474ff
Vikalp Malhotra <kimboho@gmail.com>
2021-12-13 17:37:50 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(stuff = 'roller-skate') {
+    return `This Saturday, I want to ${stuff}!`
+
+}
+
+function mondayWork(g = 'go to the office') {
+    return `This Monday, I will ${g}.`
+
+}
+
+function wrapAdjective(anonfunc = `*`) {
+    return function (stuff = 'special') {
+        return `You are ${anonfunc}${stuff}${anonfunc}!`
+
+    }
+}
+
+
+
